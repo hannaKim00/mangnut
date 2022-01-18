147c8098a84a0c99a13ebd77701e627b336e57ac
hanna <97264596+hannaKim00@users.noreply.github.com>
2022-01-18 16:40:28 +0900

    망넛이네 구현
    .

--- 망넛이네 구현/js/main.js
+++ 망넛이네 구현/js/main.js
@@ -1,14 +1,23 @@
 window.onload = function () {
-  const myPeag = document.querySelector("#sub-menu > li:nth-child(1)");
+  const myPeag = document.querySelector("#sub-menu > #my-page");
+  console.log(myPeag);
   const myPeagSub = document.querySelector("#my-peag-sub");
 
   myPeag.addEventListener("mouseenter", function() {
-    console.dir(myPeagSub);
-    if(myPeagSub.style.display == "") {
-      myPeagSub.style.display = "block";
-    }
+    myPeagSub.style.display = "block";
   });
   myPeagSub.addEventListener("mouseleave", function() {
     myPeagSub.style.display = "";
+  });
+
+  
+  const slider = document.querySelector("#best ul");
+  const slideritems = document.querySelectorAll("#best ul li");
+  const prve = document.querySelector("#but-paeg #prve");
+  const next = document.querySelector("#but-paeg #next");
+  console.log(prve);
+
+  next.addEventListener("click", function() {
+    slider.style.marginLeft = "calc(-270px + -8.9vw)";
   });
 }
