cdd790f7a0be6ae83b1a81a900dbedc06c98b2c0
hanna <97264596+hannaKim00@users.noreply.github.com>
2022-01-23 23:02:27 +0900

    망넛이네

--- 망넛이네 구현/js/main.js
+++ 망넛이네 구현/js/main.js
@@ -1,4 +1,4 @@
-// 서브메뉴 마이페이지 
+// 서브메뉴 마이페이지 ***********
 const myPeag = document.querySelector("#sub-menu > #my-page");
 const myPeagSub = document.querySelector("#my-peag-sub");
 
@@ -12,44 +12,58 @@ myPeagSub.addEventListener("mouseleave", function() {
 });
 
 
-// best 슬라이더 영역
-const slider = document.querySelector("#best ul");
+// best 슬라이더 영역 ***********
+const bestSlider = document.querySelector("#best ul");
 const sliderList = document.querySelectorAll("#best ul li");
 const sliderCount = sliderList.length; // 전체 슬라이더(li) 갯수
 const prve = document.querySelector("#but-paeg #prve");
 const next = document.querySelector("#but-paeg #next");
-let count = 0; 
-console.dir(prve);
+let click = 0; 
 
 // marginLeft 값을 바꿔주는 함수
-function moveSlider(num) {
-  slider.style.marginLeft = -num * 23 + "vw";
-  count = num;
+function moveSlider(slidername, num, marginleft) {
+  slidername.style.marginLeft = -num * marginleft + "vw";
 }
 
 // next 버튼을 클릭했을 때 발생 이벤트
 // count 값이 6-3=3 보다 작을 경우에만 실행되고 count에 + 1
 next.addEventListener("click", function() {
-  if(count < sliderCount -3) {
-    moveSlider(count + 1);
+  if(click < sliderCount -3) {
+    click++;
+    moveSlider(bestSlider, click, 23);
     prve.style.background = "url(images/main/prev.png)"
   }
   // 활성, 비활성 이미지 변경
-  if(count == 3) {
+  if(click == 3) {
     next.style.background = "url(images/main/next-hover.png)"
   }
 });
-
 // prve 버튼을 클릭했을 때 발생 이벤트
 // count 값이 0 보다 클 경우에만 실행되고 count에 - 1 
 // => next를 클릭하면 count값은 증가하기 때문에 next버튼을 클릭 했을 때만 실행가능
 prve.addEventListener("click", function() {
-  if(count > 0) {
-    moveSlider(count - 1);
+  if(click > 0) {
+    click--;
+    moveSlider(bestSlider, click, 23);
     next.style.background = "url(images/main/next.png)"
   }
   // 활성, 비활성 이미지 변경
-  if(count == 0) {
+  if(click == 0) {
     prve.style.background = "url(images/main/prev-hover.png)"
   }
 });
+
+
+// instagram *************
+const instagram = document.querySelector('#instagram-wrap ul');
+const reviewlist = document.querySelectorAll('#instagram-wrap ul li');
+const reviewlistLength = reviewlist.length; // li의 갯수
+let count = 0; 
+
+let times = setInterval(function() {
+  if(count < reviewlistLength-4) {
+    moveSlider(instagram, count, 12.1);
+    count++;
+  }
+}, 3000);
+
