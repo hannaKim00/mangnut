c3c764fb079cd0e1ab544529490aac54f71f39e8
hanna <97264596+hannaKim00@users.noreply.github.com>
2022-03-17 22:01:02 +0900

    mangnut

--- 망넛이네 구현/js/main.js
+++ 망넛이네 구현/js/main.js
@@ -4,31 +4,35 @@ const img = document.querySelector("#slider img");
 // 사이트가 로드 되었을 때 이미지 크기를 줄여줌, 헤더의 투명도를 서서히 높어줌
 window.addEventListener("load", function() {
   img.style.transform =  "scale(1)";
+  img.classList.add('transition-1_5');
   header.style.opacity = "1";
+  setTimeout(() => {
+    img.classList.remove('transition-1_5');
+  }, 1000);
 });
 
+// 햄버거 클릭시 네비 나옴 **************
+const hamburger = document.querySelector('#hamburger');
+const hamburgerList = document.querySelectorAll('#hamburger > span');
+const ul = document.querySelector('nav ul');
 
-// 스크롤 이벤트 영역
-const introduction = document.querySelector("#introduction");
-const bestWrap = document.querySelector("#best-wrap");
-const newWrap = document.querySelector("#new-wrap");
-const eventBanner = document.querySelector("#event-banner");
-const instagramWrap = document.querySelector("#instagram");
-// 스크롤이 해당값을 초과하면 내용이 나타남
-function scroll(name, num) {
-  window.addEventListener('scroll', function(){
-    if(window.scrollY > num) {
-      name.style.opacity = "1";
-      name.style.bottom = "0";
-    };
-  });
-}
-scroll(introduction,400);
-scroll(bestWrap,1200);
-scroll(newWrap,2080);
-scroll(eventBanner,2500);
-scroll(instagramWrap,2900);
-
+let on = true;
+hamburger.addEventListener('click', () => {
+  if(on) {
+    ul.style.width = '45vw';
+    hamburgerList[0].style.transform = 'translateY(12px) rotate(-45deg)';
+    hamburgerList[1].style.opacity = '0';
+    hamburgerList[2].style.transform = 'translateY(-11px) rotate(45deg)';
+    on = false;
+  } else {
+    ul.style.width = '0';
+    ul.style.width = '0';
+    hamburgerList[0].style.transform = 'translateY(0) rotate(0)';
+    hamburgerList[1].style.opacity = '1';
+    hamburgerList[2].style.transform = 'translateY(0) rotate(0)';
+    on = true;
+  }
+});
 
 // best 슬라이더 영역 ***********
 const bestSlider = document.querySelector("#best ul");
@@ -40,15 +44,16 @@ let click = 0;
 
 // marginLeft 값을 바꿔주는 함수
 function moveSlider(slidername, num, marginleft) {
-  slidername.style.marginLeft = -num * marginleft + "vw";
+  slidername.style.marginLeft = -num * marginleft + "px";
 }
 
 // next 버튼을 클릭했을 때 발생 이벤트
 // count 값이 6-3=3 보다 작을 경우에만 실행되고 count에 + 1
+let marginWidth = sliderList[0].offsetWidth;
 next.addEventListener("click", function() {
   if(click < sliderCount-3) {
     click++;
-    moveSlider(bestSlider, click, 23);
+    moveSlider(bestSlider, click, marginWidth);
     prve.style.background = "url(images/main/prev.png)"
   }
   // 활성, 비활성 이미지 변경
@@ -62,7 +67,7 @@ next.addEventListener("click", function() {
 prve.addEventListener("click", function() {
   if(click > 0) {
     click--;
-    moveSlider(bestSlider, click, 23);
+    moveSlider(bestSlider, click, marginWidth);
     next.style.background = "url(images/main/next.png)"
   }
   // 활성, 비활성 이미지 변경
@@ -70,6 +75,41 @@ prve.addEventListener("click", function() {
     prve.style.background = "url(images/main/prev-hover.png)"
   }
 });
+
+// 해상도 1024미만일 때 터치 이벤트
+const bestUl = document.querySelector("#best ul") 
+const bestUlLi = document.querySelectorAll("#best ul li") 
+let curPos = 0;
+let postion = 1;
+let start_x, end_x;
+let liLength =  bestUlLi.length;
+let imgWidth = bestUlLi[0].offsetWidth;
+
+if (matchMedia("screen and (max-width:1024px)").matches) { 
+  bestUl.addEventListener('touchstart', touch_start);
+  bestUl.addEventListener('touchend', touch_end);
+}
+
+function touch_start(event) {
+  start_x = event.touches[0].pageX;
+}
+
+function touch_end(event) {
+  end_x = event.changedTouches[0].pageX;
+  if(start_x > end_x){
+    if(curPos < liLength-2) {
+      postion -= imgWidth;
+      bestUl.style.transform = `translateX(${postion}px)`;
+      curPos = curPos + 1;
+    }
+  } else{
+      if(curPos > 0) {
+        postion += imgWidth;
+        bestUl.style.transform = `translateX(${postion}px)`;
+        curPos = curPos - 1;
+      }
+    }
+}
 
 
 // instagram *************
